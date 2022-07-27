bcc24c1f1773af2e6f7edad9b14230e59f814300
yday <yangdayeon2@gmail.com>
2022-07-27 15:38:11 +0900

    명시적 intent 데이터 전달 완성

--- app/src/main/java/kr/hs/emirim/s2106/mirim_project_0727_3/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2106/mirim_project_0727_3/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
     RadioGroup rg;
+    EditText editTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         Button btnStart = findViewById(R.id.btn_start);
         btnStart.setOnClickListener(btnStartListener);
         rg = findViewById(R.id.rg);
+        editTitle = findViewById(R.id.edit_title);
     }
 
     View.OnClickListener btnStartListener = new View.OnClickListener() {
@@ -33,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
                     break;
             }
             Intent intent = new Intent(getApplicationContext(), c);
+            intent.putExtra("title", editTitle.getText().toString());
+            startActivity(intent);
         }
     };
 }
--- app/src/main/java/kr/hs/emirim/s2106/mirim_project_0727_3/SecondActivity.java
+++ app/src/main/java/kr/hs/emirim/s2106/mirim_project_0727_3/SecondActivity.java
@@ -2,9 +2,11 @@ package kr.hs.emirim.s2106.mirim_project_0727_3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -12,6 +14,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+        TextView textTitle = findViewById(R.id.text_title);
+        textTitle.setText(title);
         Button btnPrev = findViewById(R.id.btn_prev);
         btnPrev.setOnClickListener(btnPrevListener);
     }
